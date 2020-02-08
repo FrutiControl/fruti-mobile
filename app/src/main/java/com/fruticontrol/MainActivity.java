f189808efe98569622439bde11c61b3fd85cdeb2
gitAccCarlo <cardavid1997@gmail.com>
2020-02-08 16:43:42 -0500

    Actividad de inicio

--- app/src/main/java/com/fruticontrol/MainActivity.java
+++ app/src/main/java/com/fruticontrol/MainActivity.java
@@ -2,13 +2,37 @@ package com.fruticontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtRegistro;
+    Button iniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniciarSesion=findViewById(R.id.buttonIniciarSesion);
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),AccionesActivity.class);
+                startActivity(intent);
+            }
+        });
+        txtRegistro=findViewById(R.id.textViewRegistrarse);
+        txtRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
