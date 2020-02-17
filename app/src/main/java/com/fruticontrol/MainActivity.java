947d928645b45f5dbbb21138db05f5ad0ebaca3e
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2020-02-16 21:30:36 -0500

    consumo del login de la usersAPI mediante JsonObjectRequest

--- app/src/main/java/com/fruticontrol/MainActivity.java
+++ app/src/main/java/com/fruticontrol/MainActivity.java
@@ -1,36 +1,75 @@
 package com.fruticontrol;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtRegistro;
     Button iniciarSesion;
+    TextView txtUsername;
+    TextView txtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        iniciarSesion=findViewById(R.id.buttonIniciarSesion);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPass = findViewById(R.id.txtPass);
+        iniciarSesion = findViewById(R.id.buttonIniciarSesion);
         iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(view.getContext(),AccionesActivity.class);
-                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Espere un momento por favor", Toast.LENGTH_SHORT).show();
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String body = "{\"username\":\"" + txtUsername.getText() + "\",\"password\":\"" + txtPass.getText() + "\"}";
+                Log.i("usersAPI", "Credenciales: " + body);
+                JSONObject credentials = null;
+                try {
+                    credentials = new JSONObject(body);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST,
+                        "http://10.0.2.2:8000/login/"/*TODO: cambiar a URL real para producción!!!!*/, credentials, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("usersAPI", response.toString());
+                        //TODO: extraer el token del response y verificarlo antes de la siguiente actividad
+                        /*Intent intent = new Intent(view.getContext(), AccionesActivity.class);
+                        startActivity(intent);*/
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("usersAPI", "Error en la invocación a la API " + error.getCause());
+                    }
+                });
+                queue.add(loginRequest);
             }
         });
-        txtRegistro=findViewById(R.id.textViewRegistrarse);
+        txtRegistro = findViewById(R.id.textViewRegistrarse);
         txtRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(view.getContext(),RegistroActivity.class);
+                Intent intent = new Intent(view.getContext(), RegistroActivity.class);
                 startActivity(intent);
             }
         });
