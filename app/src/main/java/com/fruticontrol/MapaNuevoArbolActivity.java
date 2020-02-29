4df90223898448870976f32e52e186b5d6ac677d
gitAccCarlo <cardavid1997@gmail.com>
2020-02-29 16:45:21 -0500

    Posibilidad de mover ubicacion del arbol en el mapa

--- app/src/main/java/com/fruticontrol/MapaNuevoArbolActivity.java
+++ app/src/main/java/com/fruticontrol/MapaNuevoArbolActivity.java
@@ -11,6 +11,9 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -32,15 +35,18 @@ public class MapaNuevoArbolActivity extends FragmentActivity implements OnMapRea
 
     static final int MY_PERMISSIONS_REQUEST_LOCATION = 100;
     static final int REQUEST_CHECK_SETTINGS = 200;
-    static final double RADIUS_OF_EARTH_KM = 6371.01;
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
-    private Marker arbol;
+    private Marker arbolMarker;
     private LocationRequest mLocationRequest;
     private LocationCallback mLocationCallback;
     double latitude;
     double longitude;
+    double altitude;
+
+    Button seleccionarUbicacionButton;
+    ImageView marcadorArbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +57,23 @@ public class MapaNuevoArbolActivity extends FragmentActivity implements OnMapRea
         mapFragment.getMapAsync(this);
         requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, "Para ver ubicación", MY_PERMISSIONS_REQUEST_LOCATION);
         mFusedLocationClient= LocationServices.getFusedLocationProviderClient(this);
+        seleccionarUbicacionButton=findViewById(R.id.buttonEscogerUbicacion);
+        marcadorArbol=findViewById(R.id.imageViewMarcadorArbol);
 
+        marcadorArbol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog(latitude,longitude);
+            }
+        });
         mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
                             Log.i("FLOCATION", "First Location update in the callback: " + location);
-                            arbol.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(arbol.getPosition()));
+                            arbolMarker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+                            arbolMarker.setVisible(false);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(arbolMarker.getPosition()));
                             mMap.moveCamera(CameraUpdateFactory.zoomTo(20));
                         }
                     }
@@ -74,13 +89,21 @@ public class MapaNuevoArbolActivity extends FragmentActivity implements OnMapRea
                     if (location.getLatitude() != latitude || location.getLongitude() != longitude) {
                         latitude = location.getLatitude();
                         longitude = location.getLongitude();
-                        arbol.setPosition(new LatLng(latitude, longitude));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(arbol.getPosition()));
+                        arbolMarker.setPosition(new LatLng(latitude, longitude));
+                        arbolMarker.setVisible(false);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(arbolMarker.getPosition()));
                         mMap.moveCamera(CameraUpdateFactory.zoomTo(20));
                     }
                 }
             }
         };
+    }
+
+
+    public void openDialog(double lat,double lng) {
+        Dialogo exampleDialog = new Dialogo();
+        exampleDialog.setText("Latitud: "+lat+"\n"+"Longitud: "+lng);
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
     }
 
 
@@ -92,9 +115,27 @@ public class MapaNuevoArbolActivity extends FragmentActivity implements OnMapRea
         LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        arbol= mMap.addMarker(new MarkerOptions().position(new LatLng(4, -72)).icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)));
-        mMap.setMapType(mMap.MAP_TYPE_SATELLITE);
+        arbolMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(4, -72)).icon(BitmapDescriptorFactory.fromResource(R.drawable.tree)).draggable(true));
+        arbolMarker.setVisible(false);
+        mMap.setMapType(mMap.MAP_TYPE_HYBRID);
+
+
+        mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                LatLng center=mMap.getCameraPosition().target;
+                if(arbolMarker!=null){
+                    mMap.clear();
+                    arbolMarker.remove();
+                    mMap.addMarker(new MarkerOptions().position(center)).setVisible(false);
+                    latitude=center.latitude;
+                    longitude=center.longitude;
+                }
+            }
+        });
     }
+
+
 
 
     private void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
@@ -134,4 +175,9 @@ public class MapaNuevoArbolActivity extends FragmentActivity implements OnMapRea
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         return mLocationRequest;
     }
+
+
 }
+
+
+    //Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
