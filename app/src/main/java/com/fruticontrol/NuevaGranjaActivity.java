cf96d0915bffdf53527835e98d765a4a9ca7b890
Diego Gustavo Guevara Bernal <diegog.guevarab@gmail.com>
2020-04-06 18:03:51 -0500

    comentario de guia

--- app/src/main/java/com/fruticontrol/NuevaGranjaActivity.java
+++ app/src/main/java/com/fruticontrol/NuevaGranjaActivity.java
@@ -10,5 +10,10 @@ public class NuevaGranjaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_granja);
+        /*TODO: para enviar la petición de creación el método es un POST, tienen que agregar un Header a la petición formado de la siguiente manera
+            headers["Authorization"] = `Token ${token}`
+            url = http://10.0.2.2:8000/farms/
+            body = {name,polygon} para polygon seguir el formato de https://docs.djangoproject.com/en/3.0/ref/contrib/gis/geos/#django.contrib.gis.geos.Polygon
+         */
     }
 }
