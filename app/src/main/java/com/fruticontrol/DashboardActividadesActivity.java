c5b9236e67a4ec07291648b70a60b78c1ed4111e
Carlos <cardavid1997@gmail.com>
2020-04-17 19:20:12 -0500

    Refactor .java y xml de "dashboardProcesos" a "dashboardActividades"
    Refactor .java y xml de "dashboardProcesos" a "dashboardActividades"

--- app/src/main/java/com/fruticontrol/DashboardActividadesActivity.java
+++ app/src/main/java/com/fruticontrol/DashboardActividadesActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DashboardProcesosActivity extends AppCompatActivity {
+public class DashboardActividadesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_procesos);
+        setContentView(R.layout.activity_dashboard_actividades);
     }
 }
