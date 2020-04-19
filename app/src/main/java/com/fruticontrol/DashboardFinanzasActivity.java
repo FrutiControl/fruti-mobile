6361b4c69130202238c4ea8ade47da08a3837c2d
gitAccCarlo <cardavid1997@gmail.com>
2020-04-18 20:09:10 -0500

    Botones nuevo ingreso y nuevo gasto en pantalla dashboard finanzas

--- app/src/main/java/com/fruticontrol/DashboardFinanzasActivity.java
+++ app/src/main/java/com/fruticontrol/DashboardFinanzasActivity.java
@@ -2,13 +2,38 @@ package com.fruticontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardFinanzasActivity extends AppCompatActivity {
+
+    private Button nuevoIngresoButton;
+    private Button nuevoGastoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_finanzas);
+
+        nuevoIngresoButton=findViewById(R.id.buttonNuevoIngresoDFinanzas);
+        nuevoGastoButton=findViewById(R.id.buttonNuevoGastoDFinanzas);
+
+        nuevoIngresoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),NuevoIngresoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nuevoGastoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),GastosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
