65166537f2568e1f7d89da9e5efe3c1e5e45a3b7
gitAccCarlo <cardavid1997@gmail.com>
2020-04-21 21:02:36 -0500

    Validacion campos en pantalla preferencias

--- app/src/main/java/com/fruticontrol/PreferenciasActivity.java
+++ app/src/main/java/com/fruticontrol/PreferenciasActivity.java
@@ -3,12 +3,42 @@ package com.fruticontrol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PreferenciasActivity extends AppCompatActivity {
+
+    private EditText valorJornalET;
+    private Button guardarPreferenciasButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferencias);
+        valorJornalET=findViewById(R.id.editTextValorJornal);
+        guardarPreferenciasButton=findViewById(R.id.buttonGuardarPreferencias);
+
+        guardarPreferenciasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validateForm()){
+                    Toast.makeText(PreferenciasActivity.this, "Es valido", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        if (TextUtils.isEmpty(valorJornalET.getText().toString())) {
+            valorJornalET.setError("Requerido");
+            valid = false;
+        }else{
+            valorJornalET.setError(null);
+        }
+        return valid;
     }
 }
