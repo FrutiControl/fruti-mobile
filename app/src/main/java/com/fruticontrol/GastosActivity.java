40e179b74f079e417c8d8f215128d5958c03a2ea
gitAccCarlo <cardavid1997@gmail.com>
2020-04-22 23:23:44 -0500

    Validacion campos pantalla nuevo gasto

--- app/src/main/java/com/fruticontrol/GastosActivity.java
+++ app/src/main/java/com/fruticontrol/GastosActivity.java
@@ -5,34 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class GastosActivity extends AppCompatActivity {
 
-    Spinner spinnerTipo;
-    Spinner spinnerSubtipo;
-    TextView tvMateriales;
-    TextView tvManoDeObra;
-    Switch switchMaterialManoDeObra;
-    EditText txtFechaGasto;
-    Calendar calGasto;
-    DatePickerDialog datePGasto;
+    private Spinner spinnerTipo;
+    private Spinner spinnerSubtipo;
+    private TextView tvMateriales;
+    private TextView tvManoDeObra;
+    private Switch switchMaterialManoDeObra;
+    private EditText txtFechaGasto;
+    private Calendar calGasto;
+    private DatePickerDialog datePGasto;
+    private EditText txtNombreProducto;
+    private EditText txtValor;
+    private Button guardarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastos);
 
+        guardarButton=findViewById(R.id.buttonGuardarGasto);
+        txtNombreProducto=findViewById(R.id.editTextNombreProducto);
+        txtValor=findViewById(R.id.editTextValor);
         txtFechaGasto=findViewById(R.id.editTextFechaGasto);
         switchMaterialManoDeObra=findViewById(R.id.switchMaterialesVsManoObra);
         tvMateriales=findViewById(R.id.textViewMateriales);
@@ -43,6 +53,15 @@ public class GastosActivity extends AppCompatActivity {
         spinnerTipo.setAdapter(spinnerAdapterTipo);
         ArrayAdapter<CharSequence> spinnerAdapterSubtipo = ArrayAdapter.createFromResource(this, R.array.TipoActividad, R.layout.spinner_item);
         spinnerTipo.setAdapter(spinnerAdapterSubtipo);
+
+        guardarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validateForm()){
+                    Toast.makeText(GastosActivity.this, "Es valido", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         switchMaterialManoDeObra.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -114,5 +133,73 @@ public class GastosActivity extends AppCompatActivity {
             TextView selectedTextView = (TextView) selectedView;
             selectedTextView.setError("Requerido");
         }
+    }
+
+    private boolean validateForm(){
+        boolean valid = true;
+
+        int selectedItemOfMySpinner = spinnerTipo.getSelectedItemPosition();
+        String actualPositionOfMySpinner = (String) spinnerTipo.getItemAtPosition(selectedItemOfMySpinner);
+        if (actualPositionOfMySpinner.equals("Seleccione el tipo de actividad...")) {
+            setSpinnerError(spinnerTipo);
+            valid = false;
+        }
+        else{
+            int selectedItemOfMySubSpinner= spinnerSubtipo.getSelectedItemPosition();
+            String actualPositionOfMySpinnerSub = (String) spinnerSubtipo.getItemAtPosition(selectedItemOfMySubSpinner);
+            if (actualPositionOfMySpinnerSub.equals("Seleccione el tipo de poda...")) {
+                setSpinnerError(spinnerSubtipo);
+                valid = false;
+            }
+            if (actualPositionOfMySpinnerSub.equals("Seleccione el tipo de fumigación...")) {
+                setSpinnerError(spinnerSubtipo);
+                valid = false;
+            }
+            if (actualPositionOfMySpinnerSub.equals("Seleccione el tipo de fertilización...")) {
+                setSpinnerError(spinnerSubtipo);
+                valid = false;
+            }
+            if (actualPositionOfMySpinnerSub.equals("Seleccione el tipo de riego...")) {
+                setSpinnerError(spinnerSubtipo);
+                valid = false;
+            }
+        }
+        if (TextUtils.isEmpty(txtFechaGasto.getText().toString())) {
+            txtFechaGasto.setError("Requerido");
+            valid = false;
+        }else{
+            String divide=txtFechaGasto.getText().toString();
+            String separated[]=divide.split(" ");
+            String aux=separated[3];
+            String data[]=aux.split("/");
+
+            Calendar cal=Calendar.getInstance();
+            cal.getTime();
+            Calendar cal2=Calendar.getInstance();
+            System.out.println("La seleccionada es "+data[2]+data[1]+data[0]);
+            cal2.set(Integer.parseInt(data[2]),Integer.parseInt(data[1])-1,Integer.parseInt(data[0]),00,00);
+            if(cal.compareTo(cal2)>0){
+                txtFechaGasto.setError(null);
+            }else{
+                txtFechaGasto.setError("La fecha debe ser la actual o anterior a la actual");
+                valid=false;
+            }
+        }
+        if (TextUtils.isEmpty(txtNombreProducto.getText().toString())) {
+            txtNombreProducto.setError("Requerido");
+            valid = false;
+        }else{
+            txtNombreProducto.setError(null);
+        }
+        if (TextUtils.isEmpty(txtValor.getText().toString())) {
+            txtValor.setError("Requerido");
+            valid = false;
+        }else{
+            txtValor.setError(null);
+        }
+
+
+
+        return valid;
     }
 }
