a338cf660400ab4b6f4588c8c249decd1d8543aa
Diego Guevara <diegog.guevarab@gmail.com>
2020-05-04 20:30:22 -0500

    regex corregida

--- app/src/main/java/com/fruticontrol/RegistroActivity.java
+++ app/src/main/java/com/fruticontrol/RegistroActivity.java
@@ -101,7 +101,7 @@ public class RegistroActivity extends AppCompatActivity {
         boolean valid = true;
         Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
         Matcher emailMatcher = emailPattern.matcher(txtEmail.getText());
-        Pattern passPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
+        Pattern passPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+_=])(?=\\S+$).{8,}$");
         Matcher passMatcher = passPattern.matcher(txtPass.getText());
         if (TextUtils.isEmpty(txtNombre.getText().toString())) {
             txtNombre.setError("Requerido");
