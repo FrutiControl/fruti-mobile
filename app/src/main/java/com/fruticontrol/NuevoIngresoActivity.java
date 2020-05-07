39339823d3100c1f3896b291a2ab9d7ca591a72f
Michael Steven Rincon Tellez <steven.0198@hotmail.com>
2020-05-07 18:49:18 -0500

    Consumo creacion ingresos
    Se creo consumo para nuevos ingresos

--- app/src/main/java/com/fruticontrol/NuevoIngresoActivity.java
+++ app/src/main/java/com/fruticontrol/NuevoIngresoActivity.java
@@ -1,10 +1,12 @@
 package com.fruticontrol;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,7 +18,19 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NuevoIngresoActivity extends AppCompatActivity {
 
@@ -29,12 +43,13 @@ public class NuevoIngresoActivity extends AppCompatActivity {
     private EditText etConceptoIngreso;
     private TextView txtTotal;
     private Button guardarNuevoIngresoButton;
+    private Token token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_ingreso);
-
+        token=(Token)getApplicationContext();
         spinnerTipoArbol = findViewById(R.id.spinnerTipoArbolNuevoIngreso);
         etCantidadCanastilla = findViewById(R.id.editTextCantidadCanastillas);
         etValorCanastilla = findViewById(R.id.editTextValorCanastilla);
@@ -50,6 +65,68 @@ public class NuevoIngresoActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (validateForm()) {
+
+                    RequestQueue queue = Volley.newRequestQueue(NuevoIngresoActivity.this);
+                    //CONCEPTO DEL INGRESO
+                    String concepto=etConceptoIngreso.getText().toString();
+                    //SE TOMA LA FECHA DE SIEMBRA Y SE CAMBIA EL FORMATO
+                    String divide = txtFechaIngreso.getText().toString();
+                    String separated[] = divide.split(" ");
+                    String aux = separated[3];
+                    String data[] = aux.split("/");
+                    String auxFecha = data[2] + "-" + data[1] + "-" + data[0];
+                    //VALOR DE CANASTILLA
+                    String valorCanastilla=etValorCanastilla.getText().toString();
+                    //CANTIDAD DE CANASTILLAS
+                    String cantidadCanastillas=etCantidadCanastilla.getText().toString();
+                    //SE TOMA EL TIPO DE ARBOL Y SE AVERIGUA LA INICIAL
+                    int selectedItemOfMySpinner = spinnerTipoArbol.getSelectedItemPosition();
+                    String actualPositionOfMySpinner = (String) spinnerTipoArbol.getItemAtPosition(selectedItemOfMySpinner);
+                    String inicial = inicialTipo(actualPositionOfMySpinner);
+                    String body = "{\"concept\":\"" + concepto + "\",\"date\":\"" + auxFecha + "\",\"value\":\"" + valorCanastilla + "\",\"quantity\":\"" + cantidadCanastillas + "\",\"fruit_type\":\"" + inicial + "\",\"recommended\":\"" + false + "\"}";
+                    Log.i("newIncomeAPI", "Nuevo ingreso: " + body);
+                    JSONObject newTree = null;
+                    try {
+                        newTree = new JSONObject(body);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    JsonObjectRequest newTreeRequest = new JsonObjectRequest(Request.Method.POST,
+                            "http://10.0.2.2:8000/money/incomes/"/*TODO: cambiar a URL real para producción!!!!*/, newTree,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.i("newIncomeAPI", response.toString());
+
+                                    if (response.has("error")) {
+                                        try {
+                                            Toast.makeText(NuevoIngresoActivity.this, response.getString("error"), Toast.LENGTH_SHORT).show();
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        finish();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("TreeAPI", "Error en la invocación a la API " + error.getCause());
+                            Toast.makeText(NuevoIngresoActivity.this, "Se presentó un error, por favor intente más tarde", Toast.LENGTH_SHORT).show();
+                        }
+                    }) {    //this is the part, that adds the header to the request
+                        @Override
+                        public Map<String, String> getHeaders() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            //params.put("Content-Type", "application/json");
+                            params.put("Authorization", "Token " + token.getToken());
+                            System.out.println("XXXXXXXXX EL TOKEN ES " + token.getToken());
+                            return params;
+                        }
+                    };
+                    queue.add(newTreeRequest);
+
                     Toast.makeText(NuevoIngresoActivity.this, "Es valido", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -154,6 +231,24 @@ public class NuevoIngresoActivity extends AppCompatActivity {
             }
         }
         return valid;
+    }
+
+    private String inicialTipo(String opcion) {
+        if (opcion.equals("Mango tommy")) {
+            return "M";
+        } else if (opcion.equals("Mango farchil")) {
+            return "F";
+        } else if (opcion.equals("Naranja")) {
+            return "N";
+        } else if (opcion.equals("Mandarina")) {
+            return "D";
+        } else if (opcion.equals("Limon")) {
+            return "L";
+        } else if (opcion.equals("Aguacate")) {
+            return "A";
+        } else {
+            return "B";
+        }
     }
 
     private void setSpinnerError(Spinner spinner) {
