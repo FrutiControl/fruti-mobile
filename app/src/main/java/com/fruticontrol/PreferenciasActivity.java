dbd25c132836f02f92eb4528d0ac5ece51641a3a
Michael Steven Rincon Tellez <steven.0198@hotmail.com>
2020-05-09 00:04:33 -0500

    Consumo modificar valor jornal
    Se modifica el valor del jornal en la pantalla preferencias

--- app/src/main/java/com/fruticontrol/PreferenciasActivity.java
+++ app/src/main/java/com/fruticontrol/PreferenciasActivity.java
@@ -29,6 +29,9 @@ public class PreferenciasActivity extends AppCompatActivity {
     private EditText valorJornalET;
     private Button guardarPreferenciasButton;
     private Token token;
+    private String auxCity;
+    private String auxDepartment;
+    private String auxCountry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,9 @@ public class PreferenciasActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         Log.i("dayCost", response.toString());
                         try {
+                            auxCity=response.getString("city");
+                            auxDepartment=response.getString("department");
+                            auxCountry=response.getString("country");
                             valorJornalET.setText(response.getString("day_cost"));
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -72,7 +78,50 @@ public class PreferenciasActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (validateForm()) {
-                    Toast.makeText(PreferenciasActivity.this, "Es valido", Toast.LENGTH_SHORT).show();
+                    RequestQueue queue = Volley.newRequestQueue(PreferenciasActivity.this);
+                    String body = "{\"day_cost\":\"" + valorJornalET.getText().toString()+ "\",\"city\":\"" + auxCity+ "\",\"department\":\"" + auxDepartment+ "\",\"country\":\"" + auxCountry + "\"}";
+                    Log.i("modificateTreeAPI", "Jornal modificado: " + body);
+                    JSONObject newDayCost = null;
+                    try {
+                        newDayCost = new JSONObject(body);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String auxUrl = "http://10.0.2.2:8000/users/owner/";
+                    JsonObjectRequest newDayCostRequest = new JsonObjectRequest(Request.Method.PUT,
+                            auxUrl/*TODO: cambiar a URL real para producción!!!!*/, newDayCost,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.i("modificateDayCostAPI", response.toString());
+
+                                    if (response.has("error")) {
+                                        try {
+                                            Toast.makeText(PreferenciasActivity.this, response.getString("error"), Toast.LENGTH_SHORT).show();
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        finish();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("TreeAPI", "Error en la invocación a la API " + error.getCause());
+                            Toast.makeText(PreferenciasActivity.this, "Se presentó un error, por favor intente más tarde", Toast.LENGTH_SHORT).show();
+                        }
+                    }) {    //this is the part, that adds the header to the request
+                        @Override
+                        public Map<String, String> getHeaders() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("Content-Type", "application/json");
+                            params.put("Authorization", "Token " + token.getToken());
+                            System.out.println("XXXXXXXXX EL TOKEN ES " + token.getToken());
+                            return params;
+                        }
+                    };
+                    queue.add(newDayCostRequest);
                 }
             }
         });
