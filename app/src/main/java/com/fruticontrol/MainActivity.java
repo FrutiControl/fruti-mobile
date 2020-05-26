6b8d6ae9886746e2fa18f20fbef2e54d55bdb2ef
gitAccCarlo <cardavid1997@gmail.com>
2020-05-25 22:40:13 -0500

    Cambio regex contraseña caracteres especiales ahora son opcionales

--- app/src/main/java/com/fruticontrol/MainActivity.java
+++ app/src/main/java/com/fruticontrol/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean validateForm() {
         boolean valid = true;
         Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-        Pattern passPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+!_=*.-])(?=\\S+$).{8,25}$");
+        Pattern passPattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[\\w~@#$%^&*+=`|{}:;!.?\\\"()\\[\\]-]{8,25}$");
         Matcher emailMatcher = emailPattern.matcher(txtUsername.getText());
         Matcher passMatcher = passPattern.matcher(txtPass.getText());
 
--- app/src/main/java/com/fruticontrol/RegistroActivity.java
+++ app/src/main/java/com/fruticontrol/RegistroActivity.java
@@ -102,7 +102,7 @@ public class RegistroActivity extends AppCompatActivity {
         boolean valid = true;
         Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
         Matcher emailMatcher = emailPattern.matcher(txtEmail.getText());
-        Pattern passPattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+!_=*.-])(?=\\S+$).{8,25}$");
+        Pattern passPattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[\\w~@#$%^&*+=`|{}:;!.?\\\"()\\[\\]-]{8,25}$");
         Matcher passMatcher = passPattern.matcher(txtPass.getText());
         if (TextUtils.isEmpty(txtNombre.getText().toString())) {
             txtNombre.setError("Requerido");
@@ -116,6 +116,7 @@ public class RegistroActivity extends AppCompatActivity {
         } else if (!emailMatcher.matches()) {
             Log.e("usersAPI", "El correo no es válido");
             txtEmail.setError("Correo inválido");
+            valid=false;
         } else {
             txtEmail.setError(null);
         }
